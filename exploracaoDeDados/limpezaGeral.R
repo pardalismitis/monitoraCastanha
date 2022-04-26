86222f97ed6b977a8c8035b011f655ef66fd754c
Fernando Lima - DT <pardalismitis@gmail.com>
2022-04-26 17:55:46 -0300

    Agora funciona!

--- exploracaoDeDados/limpezaGeral.R
+++ exploracaoDeDados/limpezaGeral.R
@@ -1,12 +1,24 @@
 limpezaGeral <- function(x){
-
+  #caracteres a serem substituidos
   AccChars <- "ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ "
+  #caracteres para substituir
   RegChars <- "SZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy_"
+  temp = x
+  #procurar em todas as colunas
   
-  for(i in 1:ncol(x)) {
-    if(is.character(x[, i])){
-      x[ , i] <- chartr(AccChars, RegChars, x[ , i])
-      x[ , i] <- tolower(x[ , i])
+  for(c in 1:ncol(x)){
+    for(l in 1:nrow(x)){ 
+      #mas apenas as colunas de texto (char) =)
+      
+      if(is.character(x[l, c])){
+        #trocar todos os diacriticos
+        
+        temp[l, c] = chartr(AccChars, RegChars, x[l, c])
+        #passar tudo para minuscula
+        
+        temp[l, c] = tolower(temp[l, c])
+      }
     }
   }
+  return(temp)
 }
